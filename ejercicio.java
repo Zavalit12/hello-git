346f06e4159f11f7b42fd77c82574850b77bfb6b
willn <welfordwillian@gmail.com>
2025-07-28 11:48:15 -0500

    octavo commit

---
+++ ejercicio.java
@@ -0,0 +1,11 @@
+
+
+public class ejercicio{
+    public static void main(String[] args) {
+        int num1 = 20 ; 
+        int num2 = 30 ;
+        double promedio = num1+num2*1;
+
+        System.out.println(promedio);
+    }
+}
